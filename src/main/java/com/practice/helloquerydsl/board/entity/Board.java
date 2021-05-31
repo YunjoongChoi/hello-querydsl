8d1e650b647730fdfdd3f11ff05dc1d9e6856936
YunjoongChoi <choimory@illunex.com>
2021-05-31 11:59:59 +0900

    작성일, 수정일 관련 JPA Auditing 추가

--- src/main/java/com/practice/helloquerydsl/board/entity/Board.java
+++ src/main/java/com/practice/helloquerydsl/board/entity/Board.java
@@ -1,6 +1,7 @@
 package com.practice.helloquerydsl.board.entity;
 
 import com.practice.helloquerydsl.board.type.ArticleType;
+import com.practice.helloquerydsl.common.entity.BaseTimeEntity;
 import com.practice.helloquerydsl.user.entity.User;
 import lombok.*;
 
@@ -13,7 +14,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Board {
+public class Board extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,10 +26,6 @@ public class Board {
     private String writer;
     @Enumerated(EnumType.STRING)
     private ArticleType articleType;
-    @Column
-    private LocalDateTime registDateTime;
-    @Column
-    private LocalDateTime updateDateTime;
     @OneToOne
     @JoinColumn(name = "USER_IDX")
     private User user;
